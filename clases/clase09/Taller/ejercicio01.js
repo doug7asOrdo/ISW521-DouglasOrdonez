70d3af446986a5a868b8a0f940534d6d6d44189d
doug7asOrdo <douglasom071011@gmail.com>
2026-06-22 21:15:47 -0600

    Taller Ejercicio01_02
    Se realizan los Ejercicios 01 y 02.

---
+++ clases/clase09/Taller/ejercicio01.js
@@ -0,0 +1,11 @@
+for (let indice = 1; indice <= 30; indice++ ) {
+    if (indice % 3 === 0 && indice % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if ( indice % 3 === 0 ) {
+        console.log("Fizz");
+    } else if ( indice % 5 === 0 ) {
+        console.log("Buzz");
+    } else {
+        console.log(indice);
+    }
+}
