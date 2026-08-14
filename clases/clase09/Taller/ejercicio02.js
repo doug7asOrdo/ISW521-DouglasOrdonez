e022affc909fc97350122e1bbef1a9c04304926d
Douglas <douglasom071011@gmail.com>
2026-08-14 01:50:51 -0600

    Weeks
    Se sube el contenido de las semanas correspondientes que no se habian subido.

--- clases/clase09/Taller/ejercicio02.js
+++
@@ -1,8 +0,0 @@
-const numeros = [4, 8, 15, 16, 23, 42];
-let suma = 0;
-
-for (const numero of numeros) {
-    suma += numero;
-}
-
-console.log(suma);
---
+++ clases/clase10/Taller/ejercicio02.js
@@ -0,0 +1,8 @@
+const numeros = [4, 8, 15, 16, 23, 42];
+let suma = 0;
+
+for (const numero of numeros) {
+    suma += numero;
+}
+
+console.log(suma);
